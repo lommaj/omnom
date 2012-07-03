2bd791a2beeb983c5c7e95ec577f514936bc8368
John Lomma <lommaj@gmail.com>
2012-07-03 16:31:58 -0300

    refactoring and what not being done for current project.

--- jquery.omnom.js
+++ jquery.omnom.js
@@ -2,20 +2,27 @@
 //written by John Lomma
 //5/7/2012
 /*
-need to be able to specify order of parts used for tag generation and add your own selectors for templating your tags and their lengths
+
+full page method - for html5 header/footer links can be done seperately using a selector during binding for <header> and <footer>, non html5 will have to do that in the settings passed.
+
+ajax method - needs to be able to identify if its inside a top nav or footer (find parent <footer>)
+
+if class= customLinkText = get linktext from name.
+
 */
-(function ($) {
+jQuery.analytics || (function ($) {
     //set default options
     var defaults = {
         //if querystring is passed, that will be used for analytics.  This is used for dynamic pages like products.aspx?p=d3100
+        pageName: "",
         querystring: null,
         sectionTemplate: null
-    };
+    }
 
     //------------------------------------------------------------------
     //support functions
     //------------------------------------------------------------------
-    function GetLocalVars() {        
+    function GetLocalVars() {
         var locVar = "";
         var url = location.pathname;
         var trackUrl = new Array();
@@ -29,7 +36,7 @@ need to be able to specify order of parts used for tag generation and add your o
         }
         locVar = truncSec.join('+').replace(/\+/, "");
         return locVar;
-    };
+    }
     function getUrlVars() {
         var vars = [], hash;
         var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
@@ -39,93 +46,190 @@ need to be able to specify order of parts used for tag generation and add your o
             vars[hash[0]] = hash[1];
         }
         return vars;
-    };
+    }
     function defineExitLinks() {
         //this adds a class 'external' and causes the external link to open in a new tab/window
         $("a[href*='http://']:not([href*='" + location.hostname + "']),[href*='https://']:not([href*='" + location.hostname + "'])")
         .addClass("external")
         .attr("target", "_blank");
+
     }
     //------------------------------------------------------------------
     //support functions
     //------------------------------------------------------------------
-    
 
-    $.omnom = function (options) {
+    $.analytics = function (options) {
         var options = $.extend(defaults, options);
-
-        var localVars = GetLocalVars();
+        //fire page name
+        s.pageName = options.pageName;
+        s.t();
         defineExitLinks();
-        $('a').live('click', function () {
-            var $this = $(this);
-            //string that will be submitted to omniture.            
-            var _analyticString = "";
-
-            //setup variables for each link
-            var linkCode = $this.attr('name');
-            var linkText = $this.text();
-            //remove white space
-            linkText = $.trim(linkText);
-            //if [name] attribute is empty, use title
-            if (linkText.length == 0) {
-                linkText = $this.children('[title]').attr('title') + "+image";
+        $('a').live('click', function (e) {
+            if (doAnaltics($(this), options)) {
+                return true;
+            } else {
+                e.preventDefault();
+                return false;
             }
-            linkText = (linkText != null ?
+        }); //end $('a').live();
+    } //end $.analytics()
+    $.customLinkType = function (object, linkType, linkText, options) {
+        //should add options for type: custom, external, etc all that can be called from script.  options should be optional.
+        doAnalticsCustom(object, linkType, linkText, options);
+
+    }
+    function doAnalticsCustom(object, linkType, linkText, options) {
+        var _analyticString = "";
+        var localVars = GetLocalVars();
+        //setup variables for each link
+        var $this = object;
+
+        //NEED TO REFACTOR FROM HERE
+
+        var linkText = linkText;
+        //remove white space
+        linkText = $.trim(linkText);
+
+        //if [name] attribute is empty, use title
+        if (linkText.length == 0) {
+            linkText = 'invalid string';
+        }
+        linkText = (linkText != null ?
                     linkText.replace(/ /g, '+').replace(/&/g, 'and').replace(/[^a-zA-Z 0-9:\.\/_-]/g, '_').replace(/__/g, '').toLowerCase() :
                         "noLinkText");
 
-            if (linkCode) {
-                linkCode = linkCode.replace(/ /g, '_').replace(/&/g, 'and').replace(/[^a-zA-Z 0-9:\.\/+_-]/g, '_').replace(/__/g, '').toLowerCase();
-                linkText = linkText + '+' + linkCode;
-            }
-            // only first 100 chars
-            linkText = linkText.substr(0, 100)
+        // only first 100 chars
+        linkText = linkText.substr(0, 100)
+
+        if (options != null && options != 'undefined') {
 
             if (options.querystring != null) {
                 var queryStringValue = getUrlVars()[options.querystring];
                 //check to see querystring is defined and filled
                 if (typeof queryStringValue === "undefined" || queryStringValue.length == 0) { } else { localVars = localVars + "+" + queryStringValue.substr(0, 5); }
             }
+        }
+        //finalize analytic string
+        //_analyticString = (options.pageName.length > 0) ? options.pageName + '+' : '';
+        _analyticString = localVars + '||' + linkText;
+        //console.log(_analyticString);
+        //detect link type
+        //var thisLink = $this.attr('href');
 
-            //add section templating options            
-            if (options.sectionTemplate != null) {
-                for (i = 0; i < options.sectionTemplate.length; i++) {
-                    if ($this.closest(options.sectionTemplate[i].selector).length) {
-                        linkText = linkText + "+" + options.sectionTemplate[i].reportingValue;
-                    }
+
+        //RE: REFACTOR TO HERE make a getlinktext function. kthanks
+
+
+        //check custom link
+        //NEED TO CHECK IF THIS STARTS WITH # SO #Jumps WORK!
+        if (linkType == "custom") {
+            setTimeout(function () {
+                s.tl($this, 'o', _analyticString);
+            }, 0);
+            return false;
+        }
+        else if (linkType == "external") {
+            s.tl($this, 'e', _analyticString);
+            return true;
+        }
+        //check exit link
+        else if (linkType == 'media') {
+            s.tl($this, 'o', _analyticString);
+            //var l = $(this).attr('href'); ??  need to do forwarding option
+            //setTimeout(window.location = l, 200);
+            //console.log("external");
+            return false;
+        }
+        //check sprop link exists
+        /*else if(linkType == 'sprop'){
+        var newHref = ($this.attr('href').indexOf("?") >= 0) ? $this.attr('href') + "&lid=" + _analyticString : $this.attr('href') + "?lid=" + _analyticString;
+        //console.log("lid");
+        window.location = newHref;
+        return false;
+        }
+        need a different $function all together for sprops.
+        */
+    }
+    function doAnaltics(_this, options) {
+        var $this = _this;
+
+        //string that will be submitted to omniture.            
+        var _analyticString = "";
+        var localVars = GetLocalVars();
+        //setup variables for each link
+        var linkCode = $this.attr('name');
+        var linkText = $this.text();
+        //remove white space
+        linkText = $.trim(linkText);
+        //if [name] attribute is empty, use title
+        if (linkText.length == 0) {
+            linkText = $this.children('[alt]').attr('alt') + "+image";
+        }
+        linkText = (linkText != null ?
+                    linkText.replace(/ /g, '+').replace(/&/g, 'and').replace(/[^a-zA-Z 0-9:\.\/_-]/g, '_').replace(/__/g, '').toLowerCase() :
+                        "noLinkText");
+
+        if (linkCode) {
+            linkCode = linkCode.replace(/ /g, '_').replace(/&/g, 'and').replace(/[^a-zA-Z 0-9:\.\/+_-]/g, '_').replace(/__/g, '').toLowerCase();
+            linkText = linkText + '+' + linkCode;
+        }
+        // only first 100 chars
+        linkText = linkText.substr(0, 100)
+
+        if (options.querystring != null) {
+            var queryStringValue = getUrlVars()[options.querystring];
+            //check to see querystring is defined and filled
+            if (typeof queryStringValue === "undefined" || queryStringValue.length == 0) { } else { localVars = localVars + "+" + queryStringValue.substr(0, 5); }
+        }
+
+        //add section templating options            
+        if (options.sectionTemplate != null) {
+            for (i = 0; i < options.sectionTemplate.length; i++) {
+                if ($this.closest(options.sectionTemplate[i].selector).length) {
+                    linkText = linkText + "+" + options.sectionTemplate[i].reportingValue;
                 }
             }
-            //finalize analytic string
-            _analyticString = localVars + '--' + linkText;
+        }
 
-            //detect link type
-            var thisLink = $this.attr('href');
+        //finalize analytic string
+        //_analyticString = (options.pageName.length > 0) ? options.pageName + '+' : '';
+        _analyticString = localVars + '||' + linkText;
+        //console.log(_analyticString);
+        //detect link type
+        var thisLink = $this.attr('href');
 
-            //check custom link
-            if (thisLink == "#") {
-                //console.log(_analyticString);
-                s.tl(this,'o',_analyticString);
-            }
-            //check exit link
-            else if ($this.hasClass('external')) {
-                //console.log(_analyticString);
-                s.tl(this,'e',_analyticString);
-            }
-            //check sprop link exists
-            else {
-               
-                var newHref = ($this.attr('href').indexOf("?") >= 0) ? $this.attr('href') + "&lid=" + _analyticString : $this.attr('href') + "?lid=" + _analyticString;
-                
-                window.location = newHref;
-            }
+        //check custom link
+        //NEED TO CHECK IF THIS STARTS WITH # SO #Jumps WORK!
+        if (thisLink == "#" || $this.hasClass('telephone')) {
+            setTimeout(function () {
+                s.tl($this, 'o', _analyticString);
+            }, 0);
+            //console.log("custom");           
             return false;
-        }); //end $('a').live();
-    }; //end $.analytics() 
+        }
+        else if ($this.hasClass('external')) {
+            s.tl($this, 'e', _analyticString);
+            return true;
+        }
+        //check exit link
+        else if ($this.hasClass('media')) {
+            s.tl($this, 'o', _analyticString);
+            var l = $this.attr('href');
+            setTimeout(function () { window.location = l }, 500);
+            //console.log("external");
+            return false;
+        }
+        //check sprop link exists
+        else {
+            var newHref = ($this.attr('href').indexOf("?") >= 0) ? $this.attr('href') + "&lid=" + _analyticString : $this.attr('href') + "?lid=" + _analyticString;
+            //console.log("lid");
+            window.location = newHref;
+            return false;
+        }
+    }
 
 
-
-
-    /* for ajax data i.e: $('#somesection').analytics() or any other chainable/selector scenario 
+    /* for ajax data i.e: $('#somesection').analytics() or any other chainable/selector scenario might not need it because its inside a .live() ? 
     $.fn.analytics = function (options) {
     var options = $.extend(defaults, options);
 
